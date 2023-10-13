b6c596389ceac0d45374d806a9e723434e9e9d6c
Aiko Coanaya <78509780+aikocny@users.noreply.github.com>
2023-10-12 23:13:59 -0700

    Fix prog2
    XOR, array indexing, and detecting one/two bit error

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import prog2
 
 # print( prog2.solve(0b1010_1010_0101_1011) )
 
-result = prog2.solve(0b1010_1010_0101_1011)
-expected = [0,1,0,0,0,1,0,1,0,1,0,1,0,1,0,1]
+# # Changed last bit p0
+# result = prog2.solve(0b1010_1010_0101_1011)
+# expected = [0,1,0,0,0,1,0,1,0,1,0,1,0,1,0,1]
 
+# # No issue
+# result = prog2.solve(0b0011_0000_0101_1001)
+# expected = [0,0,0,0,0,1,0,1,0,0,0,0,1,1,0,0]
+
+# First bit b11 0 to 1
+result = prog2.solve(0b1011_0000_0101_1001)
+expected = [0,1,0,0,0,1,0,1,0,0,0,0,1,1,0,0]
+
+
+print('result is ' + str(result))
 print(result == expected)
--- prog2.py
+++ prog2.py
@@ -1,35 +1,53 @@
 def solve(bits):
 
     # Extract the bits
-    original_data = [(bits >> i) & 1 for i in range(15, -1, -1)]
     data = [(bits >> i) & 1 for i in range(15, -1, -1)]
-    
-    # Compute expected parities
-    p8 = sum(data[i] for i in [7, 9, 10, 11, 12, 13, 14]) % 2
-    p4 = sum(data[i] for i in [7, 8, 11, 12, 13]) % 2
-    p2 = sum(data[i] for i in [7, 8, 9, 14, 15]) % 2
-    p1 = sum(data[i] for i in [7, 9, 11, 13, 15]) % 2
-    p0 = (sum(data[7:]) + p4 + p2 + p1) % 2
 
-    # Calculate parity issue
+    data.reverse()
+
+    # Compute expected parities
+    p8 = 0
+    for i in [15, 14, 13, 12, 11, 10, 9]:
+        p8 ^= data[i]
+    
+    p4 = 0
+    for i in [15, 14, 13, 12, 7, 6, 5]:
+        p4 ^= data[i]
+    
+    p2 = 0
+    for i in [15, 14, 11, 10, 7, 6, 3]:
+        p2 ^= data[i]
+
+    p1 = 0
+    for i in [15, 13, 11, 9, 7, 5, 3]:
+        p1 ^= data[i]
+
+    p0 = p8 ^ p4 ^ p2 ^ p1
+    for i in [15, 14, 13, 12, 11, 10, 9, 7, 6 , 5, 3]:
+        p0 ^= data[i]
+
+    # Check discrepancy
+    p8 = 1 if p8 != data[8] else 0
+    p4 = 1 if p4 != data[4] else 0
+    p2 = 1 if p2 != data[2] else 0
+    p1 = 1 if p1 != data[1] else 0
+    p0 = 1 if p0 != data[0] else 0
+
+    # print('p8 p4 p2 p1 p0 ' + str(p8) + str(p4) + str(p2) + str(p1) + str(p0))
     parity_issue = (p8 << 3) | (p4 << 2) | (p2 << 1) | p1
     
-    # If parity issue is not zero, it points to the error bit
-    if parity_issue != 0:
-        # Correct the error
-        data[parity_issue-1] = 1 - data[parity_issue-1]
-    
-    # Check if error is corrected or still there (two errors)
-    corrected_p0 = (sum(data[7:]) + p4 + p2 + p1) % 2
-    if corrected_p0 == data[0]:
-        if parity_issue == 0:
-            flag = [0, 0]  # No errors
-        else:
-            flag = [0, 1]  # One error
+    if parity_issue == 0 and p0 == 0:
+        flag = [0, 0]  # No errors
+    elif parity_issue == 0 and p0 == 1:
+        flag = [0, 1]  # One error
+        data[parity_issue] ^= 1
+    elif p0 == 0:
+        flag = [1, 0] #Two errors
     else:
-        flag = [1, 0]  # Two errors
-    
+        flag = [0, 1]  # One error
+        data[parity_issue] ^= 1 # Fix error
+
     # Extract the original 11-bit message
-    message = [original_data[0], original_data[1], original_data[2], original_data[3], original_data[4], original_data[5], original_data[6], original_data[8], original_data[9], original_data[10], original_data[12]]
+    message = [data[3], data[5], data[6], data[7], data[9], data[10], data[11], data[12], data[13], data[14], data[15]]
     
     return flag + [0, 0, 0] + message
