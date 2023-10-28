cd5444f8c80b00fd8089b978ff8e89cdc27285a6
Tyler Le <tylerle99@yahoo.com>
2023-10-28 14:23:57 -0700

    assembler v2

--- project/assembler/assembler.py
+++ project/assembler/assembler.py
@@ -1,10 +1,13 @@
+import argparse
+
 """
 Instruction            | Type | Opcode | rs   | rt   | rd   | imm    | target
 -----------------------|------|--------|------|------|------|--------|-------
 xor                    |  R   | 000    | XXX  | XXX  | -    | -      | -
 beq                    |  I   | 001    | XX   | -    | XX   | XX     | -
-add                    |  I   | 010    | XXX  | -    | -    | XXX    | -
-lsl                    |  I   | 100    | XXX  | -    | -    | XXX    | -
+addi                   |  I   | 010    | XXX  | -    | -    | XXX    | -
+andi                   |  I   | 011    | XXX  | -    | -    | XXX    | -
+rls                    |  I   | 100    | XXX  | -    | -    | XXX    | -
 ld                     |  I   | 101    | XX   | -    | XX   | XX     | -
 st                     |  I   | 110    | XX   | -    | XX   | XX     | -
 j                      |  J   | 111    | -    | -    | -    | -      | XXXXXX
@@ -13,62 +16,120 @@ j                      |  J   | 111    | -    | -    | -    | -      | XXXXXX
 INSTRUCTION_FORMAT = {
     "xor": {"type": "R", "opcode": "000", "width": 3},
     "beq": {"type": "I", "opcode": "001", "width": 2},
-    "add": {"type": "I", "opcode": "010", "width": 3},
-    "lsl": {"type": "I", "opcode": "011", "width": 3},
+    "addi": {"type": "I", "opcode": "010", "width": 3},
+    "andi": {"type": "I", "opcode": "011", "width": 3},
+    "rls": {"type": "I", "opcode": "100", "width": 3},
     "ld":  {"type": "I", "opcode": "101", "width": 2},
     "st":  {"type": "I", "opcode": "110", "width": 2},
     "j":   {"type": "J", "opcode": "111", "width": 6}
 }
 
-def assemble(instr):
-    tokens = instr.replace(",", "").split()
-    op = tokens[0]
-    opcode = INSTRUCTION_FORMAT[op]["opcode"]
-    width = INSTRUCTION_FORMAT[op]["width"]
-    
-    # R-Type Instruction
-    if INSTRUCTION_FORMAT[op]["type"] == "R":
-        rs, rt = tokens[1], tokens[2]
-        return opcode + get_code(rs, width) + get_code(rt, width)
-    
-    # I-Type Instructions
-    elif INSTRUCTION_FORMAT[op]["type"] == "I":
-        if op in ["beq", "ld", "st"]:
-            rs, rd, imm = tokens[1], tokens[2], tokens[3]
-            return opcode + get_code(rs, width) + get_code(rd, width) + get_code(imm, width)
-        else:
-            rs, imm = tokens[1], tokens[2]
-            return opcode + get_code(rs, width) + get_code(imm, width)
-    
-    # J-type 
-    else:
-        target = tokens[1]
-        return opcode + target
-    
-"""
-Supports decoding of registers and immediate values.
-"""
-def get_code(val, width):
-    if val.startswith("r"):
-        val = val.replace("r", "")
-    return format(int(val), f'0{width}b')
-    
+# Clean up instruction
+def tokenize_instruction(instruction):
+    return instruction.replace(",", "").split()
 
+# Convert register or imm into binary
+def decode_value(value, width):
+    try:
+        if value.startswith("r"):
+            value = value.replace("r", "")
+        bin_value = format(int(value), f'0{width}b')
+        if len(bin_value) > width:
+            raise ValueError(f"Value '{value}' too large for specified width of {width}.")
+        return bin_value
+    
+    except ValueError as ve:
+        raise ValueError(f"Failed to decode value '{value}': {str(ve)}")
+
+# Process R-Type
+def assemble_r_type(tokens, opcode, width):
+    rs, rt = tokens[1], tokens[2]
+    return opcode + decode_value(rs, width) + decode_value(rt, width)
+
+# Process the two types of I-Type instructions
+def assemble_i_type(tokens, opcode, width):
+    rs = tokens[1]
+    if len(tokens) == 4:
+        rd, imm = tokens[2], tokens[3]
+        return opcode + decode_value(rs, width) + decode_value(rd, width) + decode_value(imm, width)
+    else:
+        imm = tokens[2]
+        return opcode + decode_value(rs, width) + decode_value(imm, width)
+
+# Process J-Type
+def assemble_j_type(tokens, opcode):
+    target = tokens[1]
+    return opcode + target
+
+# Convert assembly into machine code
+def assemble_instruction(instruction):
+    try:
+        tokens = tokenize_instruction(instruction)
+        op_info = INSTRUCTION_FORMAT.get(tokens[0])
+        if not op_info:
+            raise ValueError(f"Invalid instruction '{tokens[0]}'.")
+        
+        opcode = op_info["opcode"]
+        width = op_info["width"]
+    
+        if op_info["type"] == "R": return assemble_r_type(tokens, opcode, width)
+        elif op_info["type"] == "I": return assemble_i_type(tokens, opcode, width)
+        else: return assemble_j_type(tokens, opcode)
+        
+    except Exception as e:
+        raise ValueError(f"Failed to assemble instruction '{instruction}': {str(e)}")
+
+
+# To perform tests, run `python3 assembler.py -t`
 def test_assembler():
-    # R-Type
-    assert assemble("xor r0 r1") == "000000001"
-    
-    # I-Type
-    assert assemble("beq r1 r0 2") == "001010010"
-    assert assemble("add r0 1") == "010000001"
-    assert assemble("lsl r0 2") == "011000010"
-    assert assemble("ld r0 r1 0") == "101000100"
-    assert assemble("st r0 r1 0") == "110000100"
-    
-    # J-Type
-    assert assemble("j 000000") == "111000000"
-    assert assemble("j 111111") == "111111111"
-    
+    test_data = [
+        ("xor r0 r1", "000000001"),
+        ("beq r1 r0 2", "001010010"),
+        ("addi r0 1", "010000001"),
+        ("andi r0 1", "011000001"),
+        ("rls r0 2", "100000010"),
+        ("ld r0 r1 0", "101000100"),
+        ("st r0 r1 0", "110000100"),
+        ("j 000000", "111000000"),
+        ("j 111111", "111111111")
+    ]
+
+    for instr, expected in test_data:
+        assert assemble_instruction(instr) == expected
+
     print("All tests passed.")
+
+"""
+Input - A file called `assembly.txt` with the assembly instr. separated by newline
+Output - A file called `mach_code.txt` with the corresponding machine code
+"""
+def process_file(input_filename, output_filename):
+    try:
+        with open(input_filename, 'r') as infile:
+            instructions = infile.readlines()
     
-test_assembler()
+        machine_codes = [assemble_instruction(instr.strip()) for instr in instructions]
+    
+        with open(output_filename, 'w') as outfile:
+            for code, instr in zip(machine_codes, instructions):
+                outfile.write(f"{code} \t// {instr.strip()}\n")
+                
+    except FileNotFoundError:
+        print(f"Error: File '{input_filename}' not found.")
+        
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Assembler script.")
+    parser.add_argument("-t", "--test", action="store_true", help="Run the assembler tests.")
+    args = parser.parse_args()
+
+    if args.test:
+        try:
+            test_assembler()
+            print("All tests passed.")
+        except AssertionError as e:
+            print(f"Test Failed: {str(e)}")
+    else: 
+        process_file("assembly.txt", "mach_code.txt")
