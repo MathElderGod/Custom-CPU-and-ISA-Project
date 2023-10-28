b84c716d282c465c22c21985f89f5acb070bd1dd
Alexander G. Arias <64283380+MathElderGod@users.noreply.github.com>
2023-10-28 15:34:58 -0700

    Update syntax, and renamed PC.sv to pc.sv

---
+++ project/verilog/pc.sv
@@ -0,0 +1,17 @@
+// Program Counter
+
+module pc #(parameter D=6)(
+                            input                 reset,		 // synchronous reset
+                                                  clk,       
+                                                  jumpEn,   // abs. jump enable
+                            input        [D-1:0]  target,	   // where to jump  
+                            output logic [D-1:0]  programCounter
+);
+  always_ff @(posedge clk)
+    if (reset)
+      programCounter <= 'b0;
+    else if (jumpEn)
+      programCounter <= target;
+    else
+      programCounter <= programCounter + 'b1;
+endmodule
